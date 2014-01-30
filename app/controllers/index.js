c5a5719f0661fdd5dff83dc01b94c67b414534d8
Rob Erskine <Rob@RobErskine.com>
2014-01-29 23:47:49 -0500

    fixed index controller to show home view
    Signed-off-by: Rob Erskine <Rob@RobErskine.com>

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,7 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-    Article = mongoose.model('Article');
-
-exports.article = function(req, res, next, id) {
-    Article.load(id, function(err, article) {
-        if (err) return next(err);
-        if (!article) return next(new Error('Failed to load article ' + id));
-        req.article = article;
-        next();
+exports.render = function(req, res) {
+    res.render('index', {
+        user: req.user ? JSON.stringify(req.user) : 'null'
     });
-};
-
-exports.show = function(req, res) {
-    res.jsonp(req.article);
 };
